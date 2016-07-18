f16593c8dc6144ddbb55257b6199aeec1d3ffc2f
gffuma <fumagalli.gf@gmail.com>
2016-07-18 14:56:47 +0200

    AppController set up

--- client/app/controllers/app-controller.js
+++ client/app/controllers/app-controller.js
@@ -1,13 +1,46 @@
-function AppCtrl (baseServerUrl) {
+function AppCtrl ($scope, DataService, $auth, $rootScope, Entities, Preferences, HttpUtils) {
+  console.info('AppController.');
 
-  console.log('Hey i am app controller!', baseServerUrl);
+  this.logged = () => $auth.isAuthenticated();
 
-  //this.something = "Hello world";
-  //this.methodExample = () => {
-    //alert("hey");
-  //}
+  $rootScope.entitiesBootstrapError = null;
 
-  //DataService.me.get();
+  $rootScope.$on('Entities:shopChanged', (evt, shop) => {
+    this.shop = shop;
+  })
+
+  $rootScope.$on('Entities:userChanged', (evt, user) => {
+    this.user = user;
+  })
+
+  $rootScope.$on('Entities:loadUserError', (evt, error) => {
+    $rootScope.entitiesBootstrapError = HttpUtils.makeErrorMessage(error);
+  });
+
+  $rootScope.$on('Entities:loadShopError', (evt, error) => {
+    $rootScope.entitiesBootstrapError = HttpUtils.makeErrorMessage(error);
+  });
+
+  $rootScope.$on('Entities:invalidShop', (evt, error) => {
+    Preferences.clearPreferences();
+  });
+
+  $rootScope.$on('unauthorized', (evt, error) => {
+    $auth.logout();
+    Preferences.clearPreferences();
+  });
+
+  $rootScope.$on('Entities:bootstrapStart', (evt) => {
+    $rootScope.entitiesBootstrapError = null;
+  });
+
+  this.retryBootstrap = () => Entities.bootstrap();
+
+  this.logout = () => {
+    $auth.logout();
+    Preferences.clearPreferences();
+    $rootScope.$broadcast("app:logoutSuccess");
+  };
 }
 
 export default AppCtrl;
